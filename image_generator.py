7d55a408a6389726e69550f7df45f9dec933f073
Cadic Jean Maximilien <j.cadic@9dw-lab.com>
2019-12-27 13:29:25 +0900

    horizontal flip

--- image_generator.py
+++ image_generator.py
@@ -7,24 +7,62 @@ from math import pi
 class ImageGenerator(dict):
     def __init__(self, root):
         super(ImageGenerator, self).__init__()
+        def add_text(string, img):
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            # org
+            org = (50, 50)
+            # fontScale
+            fontScale = 1
+            # Blue color in BGR
+            color = (255, 255, 255)
+            # Line thickness of 2 px
+            thickness = 2
+            # Using cv2.putText() method
+            return cv2.putText(img, string, org, font, fontScale, color, thickness, cv2.LINE_AA)
         # Load the files
-        files = dict([(name(file).split('_')[0], file) for file in listfiles(root)])
-        prepa = NMesh(files['preparationscan'])
-        crown = NMesh(files['crown'])
+        self.files = dict([(name(file).split('_')[0], file) for file in listfiles(root)])
+        prepa = NMesh(self.files['preparationscan'])
+        crown = NMesh(self.files['crown'])
         # Set the color
         prepa.set_color([0, 200, 200])
         crown.set_color([255, 255, 0])
         setmesh = NMesh(list=[crown, prepa])
-        _img = setmesh.shot()
-        img_master = np.zeros((800, 800, 3), np.uint8)
-        img_master[200:-200, 200:-200] = 255 - _img
-        shape = img_master.shape
+
+        _img0 = setmesh.shot()
+        setmesh.rotate(axis_rotation=0, theta=pi)
+        _img1 = setmesh.shot()
+        _img1 = cv2.flip(_img1, 0)
+        img_master0 = np.zeros((800, 800, 3), np.uint8)
+        img_master0[200:-200, 200:-200] = 255 - _img0
+        img_master1 = np.zeros((800, 800, 3), np.uint8)
+        img_master1[200:-200, 200:-200] = 255 - _img1
+
         for theta in np.arange(0, 2*pi, 2*pi / 360):
             deg = int((theta / pi) * 180)
-            img = self.rotateImage(img_master, deg)
-            img[shape[0] // 2:, :] = cv2.flip(img[shape[0] // 2:, :], 0)
-            img[shape[0] // 2:, :] = cv2.flip(img[shape[0] // 2:, :], 0)
+            img0 = self.rotateImage(img_master0, deg)
+            img0 = add_text(name(root), img0)
+            img1 = self.rotateImage(img_master1, deg)
+            img = np.zeros((800, 1600, 3), np.uint8)
+            img[:, :800] = img0
+            img[:, 800:] = img1
             self.setdefault('{root}/{deg}_None.None'.format(root=root, deg=str(deg).zfill(3)), img)
+
+    def export(self, img, plyfile):
+        # Load the files
+        anta = NMesh(self.files['antagonistscan'])
+        prepa = NMesh(self.files['preparationscan'])
+        crown = NMesh(self.files['crown'])
+        # Set the color
+        anta.set_color([200, 0, 200])
+        prepa.set_color([0, 200, 200])
+        crown.set_color([255, 255, 0])
+        centroid = crown.centroid
+        deg = int(name(img).split("_")[0])
+        theta = (deg/360)*2*pi
+        setmesh = NMesh(list=[crown, anta, prepa])
+        setmesh.translate(-centroid)
+        setmesh.rotate(axis_rotation=2, theta=theta)
+        setmesh.export(plyfile)
 
     @staticmethod
     def rotateImage(image, angle):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gnutools.utils import name, parent
 import numpy as np
 from image_generator import ImageGenerator
 import argparse
-
+from tqdm import tqdm
 __NEXT_CASE__ = 82
 __PREVIOUS_IMG__ = 81
 __VALIDATE_IMG__ = 13
@@ -36,9 +36,11 @@ def process_dir(dir, dimgs):
    imgs_path = np.array(list(dimgs.keys()))
    imgs = np.array(list(dimgs.values()))
    k, validated = reload_annotations(dir, imgs_path)
-   print(validated, dir)
+   actions=np.zeros(4)
+   k_actions = 0
+   speed=1
    while True:
-      img =  imgs[k]
+      img = imgs[k]
       img = print_validate(img) if validated else img
       cv2.imshow("", img)
       key = cv2.waitKey()
@@ -51,44 +53,62 @@ def process_dir(dir, dimgs):
          else:
             validated=False
       elif key == __NEXT_IMG__:
-         k = k + 1
-         if k == len(imgs):
+         k = k + speed
+         if k >= len(imgs):
             k = 0
          validated = False
       elif key == __PREVIOUS_IMG__:
-         k = k-1
+         k = k - speed
          if k<0:
-            k = len(imgs)-1
+            k = len(imgs)-speed
          validated = False
       elif key == __NEXT_CASE__:
          return __NEXT_CASE__, None
       elif key == __PREVIOUS_CASE__:
          return __PREVIOUS_CASE__, None
+      # Update the speed
+      k_actions+=1
+      actions[k_actions%4]=key
+      if len(set(actions))==1:
+         speed = 4
+      else:
+         speed = 1
 
 
 if __name__ == '__main__':
    parser = argparse.ArgumentParser(description='Annotate 3d files from 2d projected volumes')
-   parser.add_argument('--root',default='/mnt/9DWNas-02/MITSUI/crown/crown_valid_dataset/2019/USB3')
+   parser.add_argument('--root', default='/mnt/9DWNas-02/MITSUI/crown/crown_valid_dataset/2018')
    args = parser.parse_args()
-   cases = os.listdir(args.root)
-   k = 0
-   while True:
-      dir = os.path.join(args.root, cases[k])
-      result, img = process_dir(dir,  ImageGenerator(dir))
-      if result == __PREVIOUS_CASE__:
-         k=k-1
-         if k<0:
-            k = len(cases)-1
-      elif result==__NEXT_CASE__:
-         k = k + 1
-         if k == len(cases):
-            print('finished')
-            break
-      else:
-         with open('annotation.txt', 'a') as f:
-            f.write(",".join([name(parent(img)), img]) + '\n')
-         print(img,' is validated')
-         k = k + 1
-         if k == len(cases):
-            k = 0
+   cases = set(os.listdir(args.root))
+   #cases_processed = set([line.split(",")[0] for line in open("annotation.txt", "r").readlines()])
+   #cases = cases.difference(cases_processed)
+   cases = list(cases)
+   k=0
+   # while True:
+   #    dir = os.path.join(args.root, cases[k])
+   #    result, img = process_dir(dir,  ImageGenerator(dir))
+   #    if result == __PREVIOUS_CASE__:
+   #       k=k-1
+   #       if k<0:
+   #          k = len(cases)-1
+   #    elif result==__NEXT_CASE__:
+   #       k = k + 1
+   #       # if k == len(cases):
+   #       #    print('finished')
+   #       #    break
+   #    else:
+   #       with open('annotation.txt', 'a') as f:
+   #          f.write(",".join([name(parent(img)), img]) + '\n')
+   #       print(img,' is validated')
+   #       k = k + 1
+   #       if k == len(cases):
+   #          k = 0
 
+   for k in tqdm(range(len(cases)), total=len(cases)):
+      dir = os.path.join(args.root, cases[k])
+      image_generator = ImageGenerator(dir)
+      result, img = process_dir(dir,  image_generator)
+    #  assert result==__VALIDATE_IMG__
+    #  with open('annotation.txt', 'a') as f:
+    #     #image_generator.export(img, "samples/{name}.ply".format(name=name(parent(img))))
+    #     f.write(",".join([name(parent(img)), img]) + '\n')
